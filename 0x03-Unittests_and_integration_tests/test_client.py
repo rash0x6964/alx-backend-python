3796ed5b5cce5bafdbf0b9db9e12ee98d68f9878
rash0x6964 <rash0x6964@gmail.com>
2024-08-26 12:41:37 +0100

    ...

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -75,6 +75,7 @@ class TestGithubOrgClient(unittest.TestCase):
         client_has_licence = gh_org_client.has_license(repo, key)
         self.assertEqual(client_has_licence, expected)
 
+
 @parameterized_class([
     {
         'org_payload': TEST_PAYLOAD[0][0],
